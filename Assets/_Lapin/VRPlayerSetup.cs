30f59b89c6c7622996cd525a186a02cab521c6a9
AxelBelin29000 <axelbelin@neuf.fr>
2026-05-20 11:48:27 +0200

    day 2

--- Assets/_Lapin/VRPlayerSetup.cs
+++ Assets/_Lapin/VRPlayerSetup.cs
@@ -1,42 +1,30 @@
 using Mirror;
 using UnityEngine;
-using Valve.VR;
 
 public class VRPlayerSetup : NetworkBehaviour
 {
-    [Header("Components to disable for remote players")]
-    public Camera vrCamera;
-    public SteamVR_PlayArea playArea; // if you have one
+    public GameObject vrCamera;
+    public GameObject controllerLeft;
+    public GameObject controllerRight;
+    public Transform headProxy;      // the sphere, child of root
+    public Transform leftProxy;      // visual for left hand
+    public Transform rightProxy;     // visual for right hand
+
+    void Update()
+    {
+        if (!isLocalPlayer) return;
+        headProxy.position = vrCamera.transform.position;
+        headProxy.rotation = vrCamera.transform.rotation;
+        leftProxy.position = controllerLeft.transform.position;
+        leftProxy.rotation = controllerLeft.transform.rotation;
+        rightProxy.position = controllerRight.transform.position;
+        rightProxy.rotation = controllerRight.transform.rotation;
+    }
 
     public override void OnStartLocalPlayer()
     {
-        // This runs only on YOUR instance of the prefab
-        vrCamera.enabled = true;
-    }
-
-    void Start()
-    {
-        if (!isLocalPlayer)
-        {
-            // Disable camera on the remote player's rig
-            vrCamera.enabled = false;
-            if (playArea != null) playArea.enabled = false;
-        }
-    }
-
-    public override void OnStartClient()
-    {
-        base.OnStartClient();
-        if (!isLocalPlayer)
-        {
-            // Disable SteamVR tracking on remote rig
-            GetComponentInChildren<SteamVR_PlayArea>()?.gameObject.SetActive(false);
-
-            foreach (var tracked in GetComponentsInChildren<SteamVR_Behaviour_Pose>())
-                tracked.enabled = false;
-
-            foreach (var hand in GetComponentsInChildren<SteamVR_Behaviour_Skeleton>())
-                hand.enabled = false;
-        }
+        vrCamera.SetActive(true);
+        controllerLeft.SetActive(true);
+        controllerRight.SetActive(true);
     }
 }
