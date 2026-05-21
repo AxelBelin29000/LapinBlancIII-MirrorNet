9217eb41719ebf550d0d2def085d056e26ef8842
AxelBelin29000 <axelbelin@neuf.fr>
2026-05-21 09:59:44 +0200

    day 3

--- Assets/_Lapin/VRPlayerSetup.cs
+++ Assets/_Lapin/VRPlayerSetup.cs
@@ -1,30 +1,88 @@
 using Mirror;
 using UnityEngine;
+using Valve.VR;
 
 public class VRPlayerSetup : NetworkBehaviour
 {
     public GameObject vrCamera;
     public GameObject controllerLeft;
     public GameObject controllerRight;
-    public Transform headProxy;      // the sphere, child of root
-    public Transform leftProxy;      // visual for left hand
-    public Transform rightProxy;     // visual for right hand
+    public Transform headProxy;
+    public Transform leftProxy;
+    public Transform rightProxy;
+    public SteamVR_Behaviour_Pose leftPose;
+    public SteamVR_Behaviour_Pose rightPose;
+    private SteamVR_RenderModel leftRenderModel;
+    private SteamVR_RenderModel rightRenderModel;
+
+    [SyncVar(hook = nameof(OnLeftTrackedChanged))]
+    bool leftTracked;
+
+    [SyncVar(hook = nameof(OnRightTrackedChanged))]
+    bool rightTracked;
+
+    void OnLeftTrackedChanged(bool oldVal, bool newVal)
+    {
+        leftProxy.gameObject.SetActive(newVal);
+        if (leftRenderModel != null) leftRenderModel.enabled = newVal;
+    }
+
+    void OnRightTrackedChanged(bool oldVal, bool newVal)
+    {
+        rightProxy.gameObject.SetActive(newVal);
+        if (rightRenderModel != null) rightRenderModel.enabled = newVal;
+    }
 
     void Update()
     {
         if (!isLocalPlayer) return;
+
         headProxy.position = vrCamera.transform.position;
         headProxy.rotation = vrCamera.transform.rotation;
-        leftProxy.position = controllerLeft.transform.position;
-        leftProxy.rotation = controllerLeft.transform.rotation;
-        rightProxy.position = controllerRight.transform.position;
-        rightProxy.rotation = controllerRight.transform.rotation;
+
+        bool isLeftTracked = leftPose != null && leftPose.isValid;
+        bool isRightTracked = rightPose != null && rightPose.isValid;
+
+        if (isLeftTracked != leftTracked)
+            CmdSetLeftTracked(isLeftTracked);
+
+        if (isRightTracked != rightTracked)
+            CmdSetRightTracked(isRightTracked);
+
+        if (isLeftTracked)
+        {
+            leftProxy.position = controllerLeft.transform.position;
+            leftProxy.rotation = controllerLeft.transform.rotation;
+        }
+
+        if (isRightTracked)
+        {
+            rightProxy.position = controllerRight.transform.position;
+            rightProxy.rotation = controllerRight.transform.rotation;
+        }
+
+        // show/hide SteamVR render model locally
+        if (leftRenderModel != null) leftRenderModel.enabled = isLeftTracked;
+        if (rightRenderModel != null) rightRenderModel.enabled = isRightTracked;
     }
+
+    [Command]
+    void CmdSetLeftTracked(bool value) => leftTracked = value;
+
+    [Command]
+    void CmdSetRightTracked(bool value) => rightTracked = value;
 
     public override void OnStartLocalPlayer()
     {
         vrCamera.SetActive(true);
         controllerLeft.SetActive(true);
         controllerRight.SetActive(true);
+
+        leftRenderModel = controllerLeft.GetComponentInChildren<SteamVR_RenderModel>();
+        rightRenderModel = controllerRight.GetComponentInChildren<SteamVR_RenderModel>();
+
+        // force initial state
+        CmdSetLeftTracked(leftPose != null && leftPose.isValid);
+        CmdSetRightTracked(rightPose != null && rightPose.isValid);
     }
 }
